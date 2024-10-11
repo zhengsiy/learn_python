cfe960e62b276202db138dd0f8343c1955107bbb
zhengsiyu <18046653170@163.com>
2024-10-11 23:21:35 +0800

    feat：第er 2次提交

--- zidian.py
+++ zidian.py
@@ -121,7 +121,7 @@ for username,user_info in users.items():
         wife.append(Full_name)
     if Full_name=='xinwang':
         hasband.append(Full_name)
-print(f"\n And there is a important infomation : {wife[0]} is {hasband[0]}'s wife")
+print(f"\n And 这是我第一次在test里面写的东西 s wife")
 
 
 
